8da63f386e6c34feda5826110c989244a97157a1
malsaud <37520137+malsaud@users.noreply.github.com>
2018-04-17 22:20:08 -0400

    Create keys.js

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+	google: {
+		clientID:'349780415882-ldqq6ejl7kuhhkk30gvq4pvqt9ek47bp.apps.googleusercontent.com',
+		clientSecret:'tGNAESqsT-Tdkw2fJMt2cnOS'
+	},
+	mongodb: {
+		dbURL: 'mongodb://me:qazasd@ds049754.mlab.com:49754/dsdata'
+	},
+	session: {
+		cookieKey: 'iamsocool'
+	}
+};
