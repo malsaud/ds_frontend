9b6ce46107b902417675290527075b99c2872bbc
Helen Cho <helencho@helens-mbp-2.devices.brown.edu>
2018-05-05 18:14:11 -0400

    filtering with densities

--- views/home.js
+++ views/home.js
@@ -59,6 +59,7 @@ function searchBars(){
 
 var intvl1 = setInterval(changeBarWidth, 3600000); //set every hour: 3600000, every min: 60,000
 // var invtl2 = setInterval(sendData, 60000);
+var bars = ["#andrews-bar", "#bh-bar", "#cit-bar", "#faunce-bar", "#hay-bar", "#jww-bar", "#ratty-bar", "#rock-bar", "#scili-bar", "#watson-bar"];
 
 function changeBarWidth() {
 	$.get('/getDensities', function(data, status) {
@@ -70,9 +71,10 @@ function changeBarWidth() {
 				var p = data.densities[i].population*100;
 				dens.push(p);
 			}
+			//SORT BEFORE DOING THIS?
 
 			//manually change width of progress depending on what db query returns (is there a more efficient way to do this)
-			$("#a-bar").css("width", dens[0]+"%");
+			$("#andrews-bar").css("width", dens[0]+"%");
 			$("#bh-bar").css("width", dens[1]+"%");
 			$("#cit-bar").css("width", dens[2]+"%");
 			$("#faunce-bar").css("width", dens[3]+"%");
@@ -81,10 +83,64 @@ function changeBarWidth() {
 			$("#ratty-bar").css("width", dens[6]+"%");
 			$("#rock-bar").css("width", dens[7]+"%");
 			$("#scili-bar").css("width", dens[8]+"%");
-			$("#w-bar").css("width", dens[9]+"%");
+			$("#watson-bar").css("width", dens[9]+"%");
 		}
 	});
 }
+
+var opt;
+function filterByDensity(option) {
+	// var sorted = dens;
+	// sorted.sort(function(a,b){return b-a});
+	var sorted = [];
+	for (var i=0; i<bars.length; i++) {
+		var s = [];
+		s.push(parseFloat($(bars[i]).css("width")));
+		var parent = bars[i].substring(0, bars[i].length-4);
+		console.log(parent);
+		s.push(parent);
+		sorted.push(s);
+		
+	}
+	opt = option;
+	sorted.sort(sortNum);
+	console.log(sorted);
+	$(".progress-h").show();
+	for (var i=0; i<bars.length; i++) {
+		$(sorted[i][1]).css("order", i);
+		console.log(sorted[i][1] + $(sorted[i][1]).css("order"));
+	}
+}
+
+function sortNum(a, b) {
+	console.log(opt);
+	if (opt === "most") {
+		if (a[0] === b[0]) {
+	        return 0;
+	    }
+	    else {
+	        return (b[0] < a[0]) ? -1 : 1;
+	    }
+	}
+	else if (opt === "least") {
+		if (a[0] === b[0]) {
+	        return 0;
+	    }
+	    else {
+	        return (a[0] < b[0]) ? -1 : 1;
+	    }
+	}
+	else if (opt === "all") {
+		if (a[1] === b[1]) {
+	        return 0;
+	    }
+	    else {
+	        return (a[1] < b[1]) ? -1 : 1;
+	    }
+	}
+    
+}
+
 
 function filterByLocation(loc) {
 	if (loc == "north") {
@@ -120,3 +176,16 @@ function filterByLocation(loc) {
 		$(".east").show();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
